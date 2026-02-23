7028b525169c5d87b8323fa2faae7a61708dfb8d
nrctx <marlon.g.viloria@gmail.com>
2026-02-23 01:03:50 -0600

    fixed possible username and format code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "s3" {
-        bucket         = "terraform-state-bucket-gvil"
-        key            = "website/terraform.tfstate"
-        region         = "us-east-2"
-        dynamodb_table = "terraform_state_locks"
-    }
+  backend "s3" {
+    bucket         = "terraform-state-bucket-gvil"
+    key            = "website/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform_state_locks"
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,12 @@ provider "aws" {
 }
 
 module "backend" {
-  source                                          = "./modules/remote_backend"
-  iam_user_name                                   = var.iam_user_name
-  bucket_name                                     = var.bucket_name
-  table_name                                      = var.table_name
+  source        = "./modules/remote_backend"
+  iam_user_name = var.iam_user_name
+  bucket_name   = var.bucket_name
+  table_name    = var.table_name
 }
 
 output "iam_user_name" {
-  value                                           = module.backend.iam_user_arn
+  value = module.backend.iam_user_arn
 }
