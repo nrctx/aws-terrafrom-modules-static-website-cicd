f4b06254865fc42bf185437572dad4a1d7592f41
nrctx <marlon.g.viloria@gmail.com>
2026-02-25 02:39:00 -0600

    terraform -reconfigure flag added.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
-terraform {
-  backend "s3" {
+ /*terraform {
+ backend "s3" {
     bucket         = "terraform-state-bucket-gvil"
     key            = "website/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform_state_locks"
   }
 }
+*/
