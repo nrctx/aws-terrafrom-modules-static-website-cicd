f789c5a070e0726c2b97952e8680ccf81f7746a0
nrctx <marlon.g.viloria@gmail.com>
2026-03-13 07:48:37 -0500

    secured the public bucket

--- terraform/modules/s3_website/main.tf
+++ terraform/modules/s3_website/main.tf
@@ -26,25 +26,8 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "website_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = ["s3:GetObject"],
-        Effect    = "Allow",
-        Resource  = "${aws_s3_bucket.website_bucket.arn}/*",
-        Principal = "*"
-      },
-    ]
-  })
-
-  depends_on = [aws_s3_bucket_public_access_block.website_bucket_public_access]
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
